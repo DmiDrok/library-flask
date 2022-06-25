abeb00c8b196c1e49b4aedc60f69321b62f3ceb4
DmiDrok <79039358+DmiDrok@users.noreply.github.com>
2022-06-25 16:39:43 +0300

    change config

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     SECRET_KEY = "so0da-fsadfj1f0-2jfew9ajfsdajfasdf043-0tr1-1jkfosdjfasdf-012iet042jjsadf"
     CSRF_ENABLED = True
     #SQLALCHEMY_DATABASE_URI = "postgresql://postgres:lokisqlpass@localhost/library"
-    SQLALCHEMY_DATABASE_URI = "postgresql://uutynbqmhlimoo:7077afef16bd755a960cae8f5e72c9d660e88e777cf79b4fc3d4f82eaf3a278b@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/d4mstbt3jk0gmt"
+    SQLALCHEMY_DATABASE_URI = "postgresql://ywyiacexzygcsx:97ec91892633bc75c977e1cb5a33d94898c8b25dda90cf8e47219c22aec2d714@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/d4d4mre08o4q87"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = "smtp.mail.ru"
