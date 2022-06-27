c3124c9e90d8d41e2f536272ff5ee321a6978540
DmiDrok <79039358+DmiDrok@users.noreply.github.com>
2022-06-27 21:48:55 +0300

    add new books and more adaptive ui

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ if os.path.exists(venv_path):
 class Config:
     SECRET_KEY = "so0da-fsadfj1f0-2jfew9ajfsdajfasdf043-0tr1-1jkfosdjfasdf-012iet042jjsadf"
     CSRF_ENABLED = True
-    #SQLALCHEMY_DATABASE_URI = "postgresql://postgres:lokisqlpass@localhost/library"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:lokisqlpass@localhost/library"
     SQLALCHEMY_DATABASE_URI = "postgresql://ywyiacexzygcsx:97ec91892633bc75c977e1cb5a33d94898c8b25dda90cf8e47219c22aec2d714@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/d4d4mre08o4q87"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = "smtp.mail.ru"
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
